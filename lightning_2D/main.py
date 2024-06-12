864b2bf6c0534b9d008f73310559a6ce6a88577a
owl0108 <takuya5115@gmail.com>
2024-06-12 11:51:43 +0200

    added logic to load checkpoint from wandb

--- lightning_2D/main.py
+++ lightning_2D/main.py
@@ -14,47 +14,62 @@ from generalist import GeneralistModel
 from dataset import MedMNISTDataModule
 
 MEDMNIST_DESCRIPTION = {
-    'pathmnist' : "Hematoxylin & eosin stained histological images: multi-class",
-    'tissuemnist': 'Human kidney cortex cell images: multi-class',
-    'octmnist': "Optical coherence tomography retinal images: multi-class",
-    'pneumoniamnist': "Pediatric chest X-Ray images: binary-class",
-    'chestmnist': "Chest frontal-view X-Ray images: multi-label, binary-class",
-    'dermamnist': "Dermatoscopic images of pigmented skin lesions: multi-class",
-    'retinamnist':"Rretina fundus images: ordinal regression",
-    'breastmnist':"breast ultrasound images: binary-class",
-    'bloodmnist':"Blood cell microscope images: multi-class",
-    'organamnist':"3D abdominal CT images, axial view: multi-class",
-    'organcmnist':"3D abdominal CT images, coronal view: multi-class",
-    'organsmnist':"3D abdominal CT images, saggital view: multi-class"
+    "pathmnist": "Hematoxylin & eosin stained histological images: multi-class",
+    "tissuemnist": "Human kidney cortex cell images: multi-class",
+    "octmnist": "Optical coherence tomography retinal images: multi-class",
+    "pneumoniamnist": "Pediatric chest X-Ray images: binary-class",
+    "chestmnist": "Chest frontal-view X-Ray images: multi-label, binary-class",
+    "dermamnist": "Dermatoscopic images of pigmented skin lesions: multi-class",
+    "retinamnist": "Rretina fundus images: ordinal regression",
+    "breastmnist": "breast ultrasound images: binary-class",
+    "bloodmnist": "Blood cell microscope images: multi-class",
+    "organamnist": "3D abdominal CT images, axial view: multi-class",
+    "organcmnist": "3D abdominal CT images, coronal view: multi-class",
+    "organsmnist": "3D abdominal CT images, saggital view: multi-class",
 }
 
+
 def main(hparams):
-    #pl.seed_everything(1)
-    if hparams.tasks == ['all']:
-        hparams.tasks = ['pathmnist', 'octmnist', 'pneumoniamnist', 'chestmnist', 'dermamnist', 'retinamnist',
-                      'breastmnist', 'bloodmnist', 'tissuemnist', 'organamnist', 'organcmnist', 'organsmnist']
-        
+    # pl.seed_everything(1)
+    if hparams.tasks == ["all"]:
+        hparams.tasks = [
+            "pathmnist",
+            "octmnist",
+            "pneumoniamnist",
+            "chestmnist",
+            "dermamnist",
+            "retinamnist",
+            "breastmnist",
+            "bloodmnist",
+            "tissuemnist",
+            "organamnist",
+            "organcmnist",
+            "organsmnist",
+        ]
+
     print(f"Datasets to be used: {hparams.tasks}")
     kwargs = hparams.__dict__
 
-    if kwargs['combine_clip_embedding']:
-        if os.path.exists('./clip_embedding/clip_embedding_dict.pth'):
-            clip_embedding = torch.load('./clip_embedding/clip_embedding_dict.pth')
+    # create clip embedding
+    if kwargs["combine_clip_embedding"]:
+        if os.path.exists("./clip_embedding/clip_embedding_dict.pth"):
+            clip_embedding = torch.load("./clip_embedding/clip_embedding_dict.pth")
         else:
             device = "cuda" if torch.cuda.is_available() else "cpu"
             clip_model, preprocess = clip.load("ViT-B/32", device=device)
             clip_embedding = {}
-            for task in kwargs['tasks']:
+            for task in kwargs["tasks"]:
                 text = MEDMNIST_DESCRIPTION[task]
                 tokens = clip.tokenize(text).to(device)
                 with torch.no_grad():
                     text_features = clip_model.encode_text(tokens)
-                    clip_embedding[task] =  text_features
+                    clip_embedding[task] = text_features
             # create a new directory if it doensn`t exist
-            os.makedirs('clip_embedding', exist_ok=True)
-            torch.save(clip_embedding, './clip_embedding/clip_embedding_dict.pth')
-        kwargs['clip_embedding'] = clip_embedding
+            os.makedirs("clip_embedding", exist_ok=True)
+            torch.save(clip_embedding, "./clip_embedding/clip_embedding_dict.pth")
+        kwargs["clip_embedding"] = clip_embedding
 
+        
     model = GeneralistModel(**kwargs)
     data_module = MedMNISTDataModule(**kwargs)
 
@@ -63,27 +78,37 @@ def main(hparams):
         dirpath="checkpoints",
         filename="best-{epoch:02d}-{val_ACC:.2f}",
         save_top_k=1,
-        mode="max"
+        mode="max",
     )
-    wandb_logger = WandbLogger(log_model=True, project='MedMNIST_lightning',
-                               name=hparams.run_name+'_'+strftime("%m-%d-%Y_%H:%M:%S"),
-                               config=hparams.__dict__)
+    wandb_logger = WandbLogger(
+        log_model=True,
+        project="MedMNIST_lightning",
+        name=hparams.run_name + "_" + strftime("%m-%d-%Y_%H:%M:%S"),
+        config=hparams.__dict__,
+    )
 
-    trainer = pl.Trainer(logger=wandb_logger,
-                         fast_dev_run=hparams.fast_dev_runs,
-                         callbacks=[checkpoint_callback],
-                         max_epochs=hparams.max_epochs,
-                         devices=hparams.devices,
-                         accelerator=hparams.accelerator,
-                         precision=hparams.precision,
-                         num_sanity_val_steps=0,
-                         log_every_n_steps=hparams.log_every_n_steps,
-                         limit_train_batches=hparams.limit_train_batches,
-                         limit_val_batches=hparams.limit_val_batches,
-                         limit_test_batches=hparams.limit_test_batches,
-                         detect_anomaly=hparams.detect_anomaly,
-                         strategy=hparams.strategy,
-                         num_nodes=hparams.num_nodes)
+     # load checkpoint
+    if kwargs["wandb_checkpoint"] is not None:
+        checkpoint_reference = kwargs["wandb_checkpoint"]
+        wandb_logger.download_artifact(checkpoint_reference, artifact_type="model")
+
+    trainer = pl.Trainer(
+        logger=wandb_logger,
+        fast_dev_run=hparams.fast_dev_runs,
+        callbacks=[checkpoint_callback],
+        max_epochs=hparams.max_epochs,
+        devices=hparams.devices,
+        accelerator=hparams.accelerator,
+        precision=hparams.precision,
+        num_sanity_val_steps=0,
+        log_every_n_steps=hparams.log_every_n_steps,
+        limit_train_batches=hparams.limit_train_batches,
+        limit_val_batches=hparams.limit_val_batches,
+        limit_test_batches=hparams.limit_test_batches,
+        detect_anomaly=hparams.detect_anomaly,
+        strategy=hparams.strategy,
+        num_nodes=hparams.num_nodes,
+    )
     trainer.fit(model, data_module)
     trainer.test(model, data_module)
 
@@ -93,35 +118,43 @@ if __name__ == "__main__":
 
     # add arguments
     parser = ArgumentParser()
-    parser.add_argument("--iter_mode", default="max_size",
-                        choices=["min_size", "max_size", "max_size_cycle", "sequential"])
+    parser.add_argument(
+        "--iter_mode",
+        default="max_size",
+        choices=["min_size", "max_size", "max_size_cycle", "sequential"],
+    )
     parser.add_argument("--fast_dev_runs", default=0, type=int)
-    parser.add_argument("--tasks", nargs='+', default=["all"])
+    parser.add_argument("--tasks", nargs="+", default=["all"])
     parser.add_argument("--accelerator", default="auto")
     parser.add_argument("--devices", default=1)
     parser.add_argument("--max_epochs", default=None, type=int)
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--data_dir", default="/scratch/jed/ishii")
     parser.add_argument("--log_every_n_steps", default=10, type=int)
-    parser.add_argument('--run_name', default='', type=str, help='run_name for wandb')
-    parser.add_argument('--limit_train_batches', default=None, type=int)
-    parser.add_argument('--limit_val_batches', default=None, type=int)
-    parser.add_argument('--limit_test_batches', default=None, type=int)
-    parser.add_argument('--precision', default='32', type=str)
-    parser.add_argument('--head', default=None, type=str)
-    parser.add_argument('--num_experts', default=20, type=int)
-    parser.add_argument('--detect_anomaly', default=False, type=bool)
-    parser.add_argument('--encoder_type', default='resnet18', type=str,
-                        choices=['resnet18', 'convnext_tiny', 'convnext_small'])
-    parser.add_argument('--strategy', default='auto', type=str)
-    parser.add_argument('--num_nodes', default=1, type=int)
-    parser.add_argument('--pretrained', default=False, type=bool)
-    parser.add_argument('--lr', default=0.0001, type=float)
-    parser.add_argument('--num_head_layers', default=1, type=int)
-    parser.add_argument('--combine_clip_embedding', default=False, type=bool)
+    parser.add_argument("--run_name", default="", type=str, help="run_name for wandb")
+    parser.add_argument("--limit_train_batches", default=None, type=int)
+    parser.add_argument("--limit_val_batches", default=None, type=int)
+    parser.add_argument("--limit_test_batches", default=None, type=int)
+    parser.add_argument("--precision", default="32", type=str)
+    parser.add_argument("--head", default=None, type=str)
+    parser.add_argument("--num_experts", default=20, type=int)
+    parser.add_argument("--detect_anomaly", default=False, type=bool)
+    parser.add_argument(
+        "--encoder_type",
+        default="resnet18",
+        type=str,
+        choices=["resnet18", "convnext_tiny", "convnext_small"],
+    )
+    parser.add_argument("--strategy", default="auto", type=str)
+    parser.add_argument("--num_nodes", default=1, type=int)
+    parser.add_argument("--pretrained", default=False, type=bool)
+    parser.add_argument("--lr", default=0.0001, type=float)
+    parser.add_argument("--num_head_layers", default=1, type=int)
+    parser.add_argument("--combine_clip_embedding", default=False, type=bool)
+    parser.add_argument("--wandb_checkpoint", default=None, type=str, help="USER/PROJECT/MODEL-RUN_ID:VERSION, load checkpoint from wandb")
 
     # Add argument to load config from a YAML file
-    parser.add_argument('--config', action=ActionConfigFile)
+    parser.add_argument("--config", action=ActionConfigFile)
 
     args = parser.parse_args()
 
